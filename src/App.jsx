c4404c42b19206db27b153b565def1bfc46bf18a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-12 21:55:00 +0600

    header component added and header jsx and css connected to each other

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Shared/Navbar/Navbar';
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
+          <Route path='/order' element={<PlaceOrder />} />
         </Routes>
     </div>
   );
