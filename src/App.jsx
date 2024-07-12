6add3881dc75b4a2482447933ff72df79208e7f1
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-12 21:50:58 +0600

    routes added for home page and the cart page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Shared/Navbar/Navbar';
+import Home from './pages/Home/Home';
+import Cart from './pages/Cart/Cart';
 
 const App = () => {
   return (
     <div className='app'>
         <Navbar /> 
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
     </div>
   );
 };
