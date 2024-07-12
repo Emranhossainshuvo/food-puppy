6cb9131c569e9ef3d2ad0bc766d718c3a6aca7dc
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-12 19:37:50 +0600

    navbar styles added

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
+import { assets } from "../../../assets/assets";
+import "./navbar.css"
 
 const Navbar = () => {
+
+    
+
+
     return (
         <div className="navbar">
-            
+            <img src={assets.logo} alt="" className="logo" />
+            <ul className="navbar-menu">
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Mobile-app</li>
+                <li>Contact us</li>
+            </ul>
+            <div className="navbar-right">
+                <img src={assets.search_icon} alt="" />
+                <div className="navbar-search-icon">
+                    <img src={assets.basket_icon} alt="" />
+                    <div className="dot"></div>
+                </div>
+                <button>Sign in</button>
+            </div>
         </div>
     );
 };
