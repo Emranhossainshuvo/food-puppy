6bcd966d7d1c5ea97aa78ccfac4da74004b70888
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-12 19:45:49 +0600

    navbar dynamic active route styles added

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -1,19 +1,20 @@
+import { useState } from "react";
 import { assets } from "../../../assets/assets";
 import "./navbar.css"
 
 const Navbar = () => {
 
-    
+    const [menu, setMenu] = useState("Home")
 
 
     return (
         <div className="navbar">
             <img src={assets.logo} alt="" className="logo" />
             <ul className="navbar-menu">
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Mobile-app</li>
-                <li>Contact us</li>
+                <li onClick={()=>setMenu("Home")} className={menu==="Home"?"active":""}>Home</li>
+                <li onClick={()=>setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</li>
+                <li onClick={()=>setMenu("Mobile-app")} className={menu==="Mobile-app"?"active":""}>Mobile-app</li>
+                <li onClick={()=>setMenu("Contact-us")} className={menu==="Contact-us"?"active":""}>Contact us</li>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
