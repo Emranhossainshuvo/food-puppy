d99dd6b35ddc7cc80960300f964889c48733403a
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-12 22:06:14 +0600

    header page styles added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,11 @@ import "./Header.css"
 const Header = () => {
     return (
         <div className="header">
-            
+            <div className="header-contents">
+                <h2>Order your favorite food here</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint voluptate perferendis debitis dolorem deleniti aspernatur libero earum placeat voluptas beatae totam molestias porro, praesentium vel nobis harum voluptatum unde nisi minima maiores fugit hic eos! Ab et perferendis deserunt labore, temporibus totam beatae commodi, nisi accusamus iusto enim modi! Minus.</p>
+                <button>View menu</button>
+            </div>
         </div>
     );
 };
