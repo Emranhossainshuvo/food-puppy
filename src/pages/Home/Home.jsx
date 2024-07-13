d5715b7810ae17c26a026e454b7dbd762d368b0f
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-13 09:52:26 +0600

    imported food list in the fooddisplay component and added food dispaly component in the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ExploreMenu from "../../Components/ExploreMenu/ExploreMenu";
 import Header from "../../Components/Header/Header";
 import "./Home.css"
+import FoodDisplay from "../../Components/FoodDisplay/FoodDisplay";
 
 const Home = () => {
 
@@ -11,6 +12,7 @@ const Home = () => {
         <div>
                 <Header />
                 <ExploreMenu category={category} setCategory={setCategory} />
+                <FoodDisplay />
         </div>
     );
 };
