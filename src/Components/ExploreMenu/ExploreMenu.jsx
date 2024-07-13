3ee0bda7d652af88ce6e48b277c1c51d1db4bee3
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-13 08:38:18 +0600

    explore menu contents added image and name

--- src/Components/ExploreMenu/ExploreMenu.jsx
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,23 @@
 import "./ExploreMenu.css"
+import {menu_list} from "../../assets/assets"
 
 const ExploreMenu = () => {
     return (
-        <div>
-            
+        <div className="explore-menu" id="explore-menu">
+            <h1>Explore our menu</h1>
+            <p className="explore-menu-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos officia sunt, laboriosam voluptates quisquam earum officiis dignissimos. Accusamus eos architecto qui inventore illo, eum dolor sapiente provident. Veritatis cum vel quasi, doloribus maxime, quos corporis ipsum voluptates rem error esse.</p>
+            <div className="explore-menu-list">
+                {
+                    menu_list.map((item, index)=>{
+                        return (
+                            <div className="explore-menu-list-item" key={index}>
+                                <img src={item.menu_image} alt="" />
+                                <p>{item.menu_name}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
