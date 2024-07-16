876c6e3932f6f69c6a7ebb8988f99d9240e1af09
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-16 17:13:04 +0600

    footer added with proper styling

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,38 @@
+import { assets } from "../../assets/assets";
 import "./Footer.css"
 
 const Footer = () => {
     return (
-        <div className='footer'>
-            
+        <div className='footer' id="footer">
+            <div className="footer-content">
+                <div className="footer-content-left">
+                    <img src={assets.logo} alt="" />
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cumque labore non repellat quaerat saepe delectus soluta ut dignissimos exercitationem? Facere illo debitis doloremque dignissimos, accusantium a perferendis corporis sunt?</p>
+                    <div className="footer-social-icons">
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
+                    </div>
+                </div>
+                <div className="footer-content-center">
+                    <h2>COMPANY</h2>
+                    <ul>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li>Delivery</li>
+                        <li>Privacy policy</li>
+                    </ul>
+                </div>
+                <div className="footer-content-right">
+                    <h2>GET IN TOUCH</h2>
+                    <ul>
+                        <li>+8801620366102</li>
+                        <li>emran.shuvo.dev@gmail.com</li>
+                    </ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 c Foodpuppy.com - All Right Reserved.</p>
         </div>
     );
 };
