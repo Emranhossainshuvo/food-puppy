f1f109a8b68cb6959a1acb09a1b004e188147a53
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-07-17 08:48:35 +0600

    login popup basic function added onlcik remove, show popup etc

--- src/Components/LoginPopup/LoginPopup.jsx
+++ src/Components/LoginPopup/LoginPopup.jsx
@@ -1,9 +1,19 @@
+import { useState } from "react";
 import "./LoginPopup.css"
+import { assets } from "../../assets/assets";
 
-const LoginPopup = () => {
+const LoginPopup = ({setShowLogin}) => {
+
+    const [currentState, setCurrentState] = useState("Register")
+
     return (
         <div className="login-popup">
-            
+                <form className="login-popup-container">
+                    <div className="login-popup-title">
+                        <h2>{currentState}</h2>
+                        <img onClick={()=>setShowLogin(false)} src={assets.cross_icon} alt="" />
+                    </div>
+                </form>
         </div>
     );
 };
