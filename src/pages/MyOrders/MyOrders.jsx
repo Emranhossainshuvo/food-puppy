24bb296a93f0bc060f14bc31633192ce448a355f
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-06 15:14:22 +0600

    created all contents of my orders page

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -2,11 +2,12 @@ import { useContext, useEffect, useState } from "react";
 import "./MyOrders.css";
 import { StoreContext } from "../../context/StoreContext";
 import axios from "axios";
+import { assets } from "../../assets/assets";
 
 const MyOrders = () => {
 
     const [data, setData] = useState([]);
-    const {url, token} = useContext(StoreContext);
+    const { url, token } = useContext(StoreContext);
 
     const fetchOrders = async () => {
         const response = await axios.post(url + "/api/order/userorders", {}, { headers: { token } });
@@ -21,8 +22,35 @@ const MyOrders = () => {
     }, [token])
 
     return (
-        <div>
-
+        <div className="my-orders">
+            <h2>My Orders</h2>
+            <div className="container">
+                {
+                    data.map((order, index) => {
+                        return (
+                            <div className="my-orders-order" key={index}>
+                                <img src={assets.parcel_icon} alt="" />
+                                <p>
+                                    {order.items.map((item, index) => {
+                                        if (index === order.items.length-1) {
+                                            return item.name+" x "+ item.quantity
+                                        }
+                                        else{
+                                            return item.name+" x " + item.quantity+", "
+                                        }
+                                    })}
+                                </p>
+                                <p>
+                                    ${order.amount}.00
+                                </p>
+                                <p>Items: {order.items.length}</p>
+                                <p><span>&#x25cf;</span><b>{order.status}</b></p>
+                                <button>Track order</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };
