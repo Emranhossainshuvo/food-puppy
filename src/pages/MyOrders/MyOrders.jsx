2909bcb81a64c363abf5c6d5395e072d6e29f39c
Emranhossainshuvo <emran.shuvo.dev@gmail.com>
2024-08-06 14:46:44 +0600

    created functionalities to fetch data from database in the client side

--- src/pages/MyOrders/MyOrders.jsx
+++ src/pages/MyOrders/MyOrders.jsx
@@ -1,9 +1,28 @@
+import { useContext, useEffect, useState } from "react";
 import "./MyOrders.css";
+import { StoreContext } from "../../context/StoreContext";
+import axios from "axios";
 
 const MyOrders = () => {
+
+    const [data, setData] = useState([]);
+    const {url, token} = useContext(StoreContext);
+
+    const fetchOrders = async () => {
+        const response = await axios.post(url + "/api/order/userorders", {}, { headers: { token } });
+        setData(response.data.data);
+        // console.log(response.data.data);
+    }
+
+    useEffect(() => {
+        if (token) {
+            fetchOrders();
+        }
+    }, [token])
+
     return (
         <div>
-                  
+
         </div>
     );
 };
